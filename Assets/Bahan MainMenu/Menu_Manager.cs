da3f65331eb80e8906ea019179cf0090aaa125d4
Rashomon <mohammad18018@mail.unpad.ac.id>
2020-01-12 16:47:21 +0700

    Main Menu

---
+++ Assets/Bahan MainMenu/Menu_Manager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Manager : MonoBehaviour
+{
+    public GameObject MenuPanel;
+    public GameObject CreditsPanel;
+    public GameObject HowtoPanel;
+
+    void Start()
+    {
+        MenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        HowtoPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayButtonClicked()
+    {
+        Application.LoadLevel("AR Game");
+    }
+    public void CreditsButtonClicked()
+    {
+        MenuPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+        HowtoPanel.SetActive(false);
+    }
+    public void HowtoButtonClicked()
+    {
+        MenuPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+        HowtoPanel.SetActive(true);
+    }
+    public void Quit_Clicked()
+    {
+        Application.Quit();
+    }
+    public void BackButtonClicked()
+    {
+        MenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        HowtoPanel.SetActive(false);
+    }
+}
